5a6c9ca1dbe59c6e687ca29735006cdbc222a323
Sheekee011 <285267050+Sheekee011@users.noreply.github.com>
2026-08-05 05:43:45 +0000

    Build permanent OpenBand profile routes

--- assets/analytics-config.js
+++ assets/analytics-config.js
@@ -1,7 +1,1 @@
-window.OPENBAND_ANALYTICS_CONFIG = Object.freeze({
-  enabled: false,
-  gaMeasurementId: "",
-  apiEndpoint: "",
-  productionHosts: ["openband.ca", "www.openband.ca"],
-  debug: false
-});
+window.OPENBAND_ANALYTICS_CONFIG = Object.freeze({"enabled":false,"gaMeasurementId":"","apiEndpoint":"","productionHosts":["openband.ca","www.openband.ca"],"debug":false});
